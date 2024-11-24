577b3e6c23711ad5810747247dbe1a2eb338b26e
Sally Fung <sallyfung2018@gmail.com>
2024-11-24 19:21:52 +0800

    feat: validate env variables in ioc

--- src/ioc/ioc_container.ts
+++ src/ioc/ioc_container.ts
@@ -5,6 +5,8 @@ import { UserService } from '../services/user.service';
 import { TokenService } from '../services/token.service';
 import { UserController } from '../controllers/user.controller';
 
+import dotenv from 'dotenv';
+
 export class IoCContainer {
     private prismaClient: PrismaClient;
     private ustRepo: UserSessionTokenRepository;
@@ -14,6 +16,11 @@ export class IoCContainer {
     private userCtlr: UserController;
 
     constructor() {
+
+        // Validate environment variables
+        dotenv.config();
+        this._validateEnvVariables();
+
         // Prisma Client
         this.prismaClient = new PrismaClient();
 
@@ -31,5 +38,19 @@ export class IoCContainer {
 
     public getUserController(): UserController {
         return this.userCtlr;
+    }
+
+    private _validateEnvVariables(): void {
+        const requiredEnvVars = [
+            'DATABASE_URL',
+            'PORT',
+            'JWT_SECRET_KEY'
+        ];
+
+        requiredEnvVars.forEach((varName) => {
+            if (!process.env[varName]) {
+                throw new Error(`Environment variable ${varName} is not set`);
+            }
+        });
     }
 }
