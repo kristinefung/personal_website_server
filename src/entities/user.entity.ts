e8276acb0819b5f7550b0d4a5aebd54f4e24e69e
Sally Fung <sallyfung2018@gmail.com>
2024-11-24 12:11:28 +0800

    refactor: user validate logic abstract to helper method to reduce duplicate

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -29,68 +29,35 @@ export class User {
     }
 
     validateCreateInput(): ValidationResult<User | undefined> {
-        // Step 1: Validate request body
-        const createUserSchema = z.object({
-            name: z
-                .string({ required_error: "name is required" })
-                .min(1, "name is required"),
-            email: z
-                .string({ required_error: "email is required" })
-                .email("Invalid email address"),
-            password: z
-                .string({ required_error: "password is required" })
-                .min(1, "password is required"),
-        });
-
-        const result = createUserSchema.safeParse(this);
-
-        // Step 2: If input invalid, return false
-        if (!result.success) {
-            const errors = result.error.errors.map(e => e.message);
-            return new ValidationResult(false, undefined, errors);
-        }
-
-        // Step 2: If input valid, return true
-        this.email = result.data.email;
-        this.name = result.data.name;
-        this.password = result.data.password;
-
-        const validatedData = this;
-        return new ValidationResult(true, validatedData);
+        return this._validateInput(z.object(
+            {
+                name: z.string({ required_error: "name is required" }).min(1, "name is required"),
+                email: z.string({ required_error: "email is required" }).email("Invalid email address"),
+                password: z.string({ required_error: "password is required" }).min(1, "password is required"),
+            }
+        ));
     }
 
     validateUpdateInput(): ValidationResult<User | undefined> {
-        // Step 1: Validate request body
-        const updateUserSchema = z.object({
-            name: z
-                .string()
-                .optional()
-                .nullable(),
-            email: z
-                .string()
-                .email("Invalid email address")
-                .optional()
-                .nullable(),
-            password: z
-                .string()
-                .optional()
-                .nullable(),
-        });
+        return this._validateInput(z.object(
+            {
+                name: z.string().optional().nullable(),
+                email: z.string().email("Invalid email address").optional().nullable(),
+                password: z.string().optional().nullable(),
+            }
+        ));
+    }
 
-        const result = updateUserSchema.safeParse(this);
+    private _validateInput(schema: z.ZodSchema): ValidationResult<User | undefined> {
+        const result = schema.safeParse(this);
 
-        // Step 2: If input invalid, return false
         if (!result.success) {
             const errors = result.error.errors.map(e => e.message);
             return new ValidationResult(false, undefined, errors);
         }
 
-        // Step 2: If input valid, return true
-        this.email = result.data.email || undefined;
-        this.name = result.data.name || undefined;
-        this.password = result.data.password || undefined;
-
-        const validatedData = this;
-        return new ValidationResult(true, validatedData);
+        // Update fields with validated data
+        Object.assign(this, result.data);
+        return new ValidationResult(true, this);
     }
 }
