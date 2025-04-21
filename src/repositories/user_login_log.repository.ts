0e2c5a5164b4f5782250a0fb2adb8e70aa32a001
Sally Fung <sallyfung2018@gmail.com>
2025-04-21 13:30:27 +0800

    refactor(auth): simplify user login log handling by removing unnecessary null checks and ensuring consistent date assignment

--- src/repositories/user_login_log.repository.ts
+++ src/repositories/user_login_log.repository.ts
@@ -23,11 +23,8 @@ export class UserLoginLogRepository implements IUserLoginLogRepository {
                 loginAt: log.loginAt ?? new Date(),
                 logoutAt: log.logoutAt,
 
-                createdAt: log.createdAt ?? new Date(),
+                createdAt: new Date(),
                 createdBy: log.createdBy ?? 0,
-                updatedAt: log.updatedAt ?? new Date(),
-                updatedBy: log.updatedBy ?? 0,
-                deleted: log.deleted ?? 0,
             },
         });
 
@@ -35,28 +32,28 @@ export class UserLoginLogRepository implements IUserLoginLogRepository {
     }
 
     async updateUserLoginLogBySessionToken(log: UserLoginLog, sessionToken: string): Promise<UserLoginLog> {
-        const createdUserLoginLog = await this.prismaClient.userLoginLog.update({
+        const updatedUserLoginLog = await this.prismaClient.userLoginLog.update({
             where: {
                 sessionToken: sessionToken,
                 deleted: 0
             },
             data: {
-                userId: log.userId ?? undefined,
-                ipAddress: log.ipAddress ?? undefined,
-                userAgent: log.userAgent ?? undefined,
-                sessionToken: log.sessionToken ?? undefined,
-                loginAt: log.loginAt ?? undefined,
-                logoutAt: log.logoutAt ?? undefined,
+                userId: log.userId,
+                ipAddress: log.ipAddress,
+                userAgent: log.userAgent,
+                sessionToken: log.sessionToken,
+                loginAt: log.loginAt,
+                logoutAt: log.logoutAt,
 
-                createdAt: log.createdAt ?? undefined,
-                createdBy: log.createdBy ?? undefined,
-                updatedAt: log.updatedAt ?? undefined,
-                updatedBy: log.updatedBy ?? undefined,
-                deleted: log.deleted ?? undefined,
+                createdAt: log.createdAt,
+                createdBy: log.createdBy,
+                updatedAt: log.updatedAt,
+                updatedBy: log.updatedBy,
+                deleted: log.deleted,
             },
         });
 
-        return createdUserLoginLog;
+        return updatedUserLoginLog;
     }
 
     async getUserLoginLogBySessionToken(sessionToken: string): Promise<UserLoginLog | null> {
