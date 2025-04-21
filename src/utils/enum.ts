8eed730dfe294b2d002f8879b6cf0943621a8402
Sally Fung <sallyfung2018@gmail.com>
2025-04-21 13:50:21 +0800

    refactor(enum): remove user role and status enums to streamline codebase

--- src/utils/enum.ts
+++ src/utils/enum.ts
@@ -1,31 +1,4 @@
 /************************************************************************
- *                           Domain related
- ************************************************************************/
-export enum UserRole {
-    ADMIN = 1,
-    USER = 2,
-    DEMO = 3,
-    SYSTEM = 9999,
-}
-
-export enum UserStatus {
-    ACTIVE = 1,
-    UNVERIFIED = 2, // Email not verified
-    PENDING = 3, // Pending admin activate account manually
-    SUSPENDED = 4, // Temporarily cannot login
-    BANNED = 5, // Permanently banned
-    LOCKED = 6, // Too many failed attempts, would store last user status
-    DELETED = 6, // Account deleted, no longer accessible
-}
-
-export enum EnquiryStatus {
-    UNHANDLED = 1,
-    HANDLED = 2,
-    IGNORED = 3,
-}
-
-
-/************************************************************************
  *                           API related
  ************************************************************************/
 export enum ApiStatusCode {
